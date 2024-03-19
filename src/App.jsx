3dfbf32b487aedd52c7188b57cdf4191f30d02ac
Andrey Svyrskyi <akkad1v@gmail.com>
2024-03-19 05:28:41 +0200

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 // import { useState } from 'react'
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 
 import "./App.css";
 
@@ -6,9 +9,9 @@ function App() {
   return (
     <div>
       <h1>Phonebook</h1>
-      {/* <ContactForm />
+      <ContactForm />
       <SearchBox />
-      <ContactList /> */}
+      <ContactList />
     </div>
   );
 }
